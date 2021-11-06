137c6f0b3759f58879508bab3d610088b23cee44
dkit150750 <dkit150750@gmail.com>
2021-11-06 16:55:24 +0300

    2

--- src/pages/Groups.vue
+++ src/pages/Groups.vue
@@ -1,13 +1,12 @@
 <template>
-<p>s</p>
+  <p>s</p>
 </template>
 
-<script async setup lang="ts">import GroupService from 'src/services/GroupService';
-import {computed} from 'vue';
+<script async setup lang="ts">
+import GroupService from 'src/services/GroupService';
+// import {computed} from 'vue';
 
 const groupsAll = await GroupService.getItems();
 // const groupsFirst = computed(() => groupsAll);
 console.log(groupsAll);
-
 </script>
-
